58849380078b662fc1b3e18afa2a90f65962cb23
pkeargle <kearg24@yahoo.com>
2020-06-18 23:54:27 -0500

    (Week-3) ** UPDATED ** Maximum Difference and Maximum substring alphabetically programs
    remove array size 0 check

--- week-3/Java/MaxDifference.java
+++ week-3/Java/MaxDifference.java
@@ -2,47 +2,56 @@
 *
 * Description:
 *  Find the maximum difference by calculating positive differences between value
-*  For example, if the given input [2, 6, 8, 5, 10, 1, 3, 4], the size is 8 and
-*  the maximum difference is 9
+*  For example, if the given input [2, 3, 10, 2, 4, 8, 1] the size is 7 and
+*  the maximum difference is 8
 *
 */
 public class MaxDifference {
 
-  public int maxDiff(int arr[], int arr_size) {
-    int max = arr[0];
-    int min = arr[0];
-    int i;
+  private int maxDiff(int[] arr, int arr_size) {
+    int maxDiff = -1;
+    int num = arr[0];
+    int difference;
 
-    // Check length of Array and return -1 if invalid
-    if (arr_size <= 1) {
-      return -1;
-    }
-
-    for (i = 1; i < arr_size; i++) {
-      // Check if each index in the array is less than min
-      if (arr[i] < min)
-          min = arr[i];
-      // Check if each index in the array is greater then max
-      if (arr[i] > max)
-          max = arr[i];
+    for (int i = 1; i < arr_size; i++) {
+      // Check if index 0 is less then next number in array
+      if (arr[i] >= num) {
+        System.out.println("current number is: " +  arr[i]);
+        // Get difference if num is larger then next number in array
+        difference = arr[i] - num;
+        System.out.println("difference is: " +  difference);
+        // Check if difference is larger then maxDiff (default is -1)
+        if (difference > maxDiff) {
+          // Set difference to maxDiff
+          maxDiff = difference;
+          System.out.println("maxdiff is: " +  maxDiff);
+        }
+      } else {
+        // num was greater then number at index 0
+        num = arr[i];
+        System.out.println("num is: " +  num);
       }
-
-    return max-min;
     }
+
+  return maxDiff;
+  }
 
   // Main
   public static void main(String[] args) {
     /** 
      * Testcases:
-     *  1. {1, 1, 1, 1} -> 0
+     *  1. {7, 9, 5, 6, 3, 2} -> 2
      *  2. {3, 4} -> 1
-     *  3. {2, 3, 10, 2, 4, 8, 1} -> 9
-     *  4. {7, 9, 5, 6, 3, 2} -> 2
-     *  5. {2} -> -1
+     *  3. {2, 3, 10, 2, 4, 8, 1} -> 8
+     *  4. {7, 1, 2, 5} -> 4
+     *  5. {7, 5, 3, 1} -> -1
+     *  6. {4} -> -1
+     *  7. {} -> java.lang.ArrayIndexOutOfBoundsException: 0
      */
-    MaxDifference maxDifference = new MaxDifference();
-    int arr[] = {1, 1, 1, 1};
+    // MaxDifference maxDifference = new MaxDifference();
+    MaxDifference md = new MaxDifference();
+    int[] arr = {2, 3, 10, 2, 4, 8, 1};
     int size = arr.length;
-    System.out.println("The Maximum Difference is " + maxDifference.maxDiff(arr, size));
+    System.out.println("The Maximum Difference is " + md.maxDiff(arr, size));
   }
 }
--- week-3/Java/MaxSubStrAlpha.java
+++ week-3/Java/MaxSubStrAlpha.java
@@ -14,27 +14,84 @@
  */
 public class MaxSubStrAlpha {
   public static String findMaxSubStringAlpha(String str) {
-    // Check if string is empty (Refer to testcases)
-    if (!str.equals("")) {
+    // Lowercase string
+    str = str.toLowerCase();
 
-      // Loop to find the maximum alphabetical substring in
-      // the substring array 
-      String s = ""; 
-      for (int i = 0; i < str.length(); ++i) { 
+    // Check if string is empty and not greater then 100 (contraints)
+    if (!str.equals("") && str.length() <= 100) {
+      // Loop to find the maximum alphabetical substring in given string
+      String s = "";
+      for (int i = 0; i < str.length(); i++) { 
         // Using the compareTo method to compare each substring in array
         // source : https://docs.oracle.com/javase/8/docs/api/java/lang/Comparable.html
         if (s.compareTo(str.substring(i)) <= 0) { 
-            s = str.substring(i); 
-            // See how we get there
-            System.out.println("The string is: " + s);
+          s = str.substring(i); 
         } 
-      } 
+        // See how we get there
+        System.out.println("The string is: " + s);
+        System.out.println("=========================================");
+      }
 
-      return s; 
+      return s;
     } else {
       throw new IllegalArgumentException("Please input a valid string.");
     }
   }
+
+  public static String bananaStrAsciiExample(String str) {
+    String charAscii = "";
+
+    // Lowercase string
+    str = str.toLowerCase();
+
+    /**
+     * For demostration purposes:
+     *   Get ascii value for each character 
+     *   Source : http://www.asciitable.com/
+     */
+    // Check if string is empty and not greater then 100 (contraints)
+    if (!str.equals("") && str.length() <= 100) {
+      System.out.println("========= Show ascii comparsion =========");
+      for (int j = 0; j < str.length(); j++) {
+        charAscii = str;
+        // Get current character
+        char currCharacter = charAscii.charAt(j);
+        if (str.indexOf(currCharacter, j) < str.length() - 1) {
+          int currAscii = (int) currCharacter;
+          System.out.println("=========================================");
+          System.out.println("Current character is: " + currCharacter + " | ascii value is: " + currAscii);
+          // Get next character
+          char nextCharacter = charAscii.charAt(j+1);
+          int nextAscii = (int) nextCharacter;
+          System.out.println("Next character is: " + nextCharacter + " | ascii value is: " + nextAscii);
+          // Check if ascii current character is greater then next
+          if (currAscii > nextAscii) {
+            int currIndex = str.indexOf(currCharacter, j);
+            char maxChar = currCharacter;
+            int maxAscii = (int) maxChar;
+            for (int k = currIndex; k < (str.length() - 1) - currIndex;  k++) {
+              nextCharacter = charAscii.charAt(k+1);
+              System.out.println("nextCharacter is: " + nextCharacter);
+              nextAscii = (int) nextCharacter;
+              if (maxAscii > nextAscii) {
+                int maxIndex = str.indexOf(maxChar);
+                System.out.println("=========================================");
+                System.out.println("Character : " + maxChar + " | ascii value is: " + maxAscii + 
+                  " and is greater then next character: " + nextCharacter + " | ascii value is: " + nextAscii);
+                System.out.println("Maximum substring is now starting at character: " + maxChar + 
+                " | index: " + maxIndex);
+              } else {
+                maxChar = nextCharacter;
+              }
+              j = k;
+            }
+          }
+        }
+      }
+    }
+    return charAscii;
+  }
+
    // Main
   public static void main(String[] args) {
     /** 
@@ -43,16 +100,15 @@ public class MaxSubStrAlpha {
      *  2. ba -> ba
      *  3. aaa -> aaa
      *  4. banana -> nana
-     *  5. basketball -> tball
+     *  5. information -> tion
      *  6. <empty> -> Please input a valid string.
      *  7. null -> return null exeception
      */
     // Test code
-    String str = null;
-    try {
-    System.out.println("The alphabetical maximim substrings are: " + findMaxSubStringAlpha(str));
-    } catch (NullPointerException e) {
-      System.out.println("NullPointerException");
-    }
+    String str = "banana";
+    // To show how maximum substring is found (ascii [a-z])
+    bananaStrAsciiExample(str);
+    System.out.println("=========================================");
+    System.out.println("The alphabetical maximim substring is: " + findMaxSubStringAlpha(str));
   }
 }
